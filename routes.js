ba2571994d383a3393b6abceebfdedc9a12b3403
Ryan Liu <dio.ryanliu@hotmail.com>
2018-11-23 16:18:35 -0800

    Adding some new stuff for website

--- routes.js
+++ routes.js
@@ -1,12 +1,9 @@
 // Routes
-
 const Utils = require("./utils");
 
 module.exports = function(app) {
 /******************************************************************************/
-    app.get("/", function(request, response) {
-        console.log("nANI");
-        console.log(Utils.testMsg);
-        response.send(Utils.testMsg);
+    app.get("/test", function(request, response) {
+        response.send("testing 1234");
     });
 };
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // server.js
-
 const express        = require("express");
 // const MongoClient    = require("mongodb").MongoClient;
 const bodyParser     = require("body-parser");
@@ -8,11 +7,14 @@ const app            = express();
 const localUrl = "mongodb://localhost:27017";
 const dbName = "mainDB";
 
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true}));
+var path = require('path');
+var STATIC_ROOT = path.resolve(__dirname, './public');
 
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true}));
 app.use(bodyParser.json({ limit: "50mb", extended: true}));
 
-require("./routes")(app)
+app.use('/', express.static(STATIC_ROOT));
+require("./routes")(app);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("We are live");
