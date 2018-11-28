a05bd29a8f3daf20bf9792cda1ece660ede728c6
Ryan Liu <dio.ryanliu@hotmail.com>
2018-11-28 14:57:37 -0800

    Starting fresh for PWA

--- routes.js
+++
@@ -1,9 +0,0 @@
-// Routes
-const Utils = require("./utils");
-
-module.exports = function(app) {
-/******************************************************************************/
-    app.get("/test", function(request, response) {
-        response.send("testing 1234");
-    });
-};
