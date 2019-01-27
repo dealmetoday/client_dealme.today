498557070514f901781b3bca5b4fc730c975192b
Alfred Hong <alf.hong91@gmail.com>
2019-01-26 16:20:28 -0800

    adjusted configs to support domain names and separate dev and prod environments

--- backend/server.js
+++ backend/server.js
@@ -63,10 +63,9 @@ init(databases);
 app.use('/api/dashboard', require("./routes/api/home/home"));
 
 if (process.env.NODE_ENV === 'production') {
-  console.log(process.env.NODE_ENV);
-  app.use(express.static(path.join(__dirname, 'dist')));
+  app.use(express.static(path.join('./dist')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './dist/index.html'));
+    res.sendFile(path.resolve('./dist/index.html'));
   });
 }
 
